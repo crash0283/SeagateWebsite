59201157b6068a50ea58c0158c6bce3e612cdd44
Chris Rasch <crasch@RFX-5-248.reelfx.com>
2016-09-16 10:49:37 -0500

    back to .php and included phpMailer

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php 
+
+    require 'php/PHPMailer/PHPMailerAutoload.php';
+
+    $mail = new PHPMailer;
+
+    //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'rasch83@gmail.com';                 // SMTP username
+    $mail->Password = 'Jbuffett83!';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->addAddress('rasch83@gmail.com', 'Chris Rasch');     // Add a recipient
+    /*$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');*/
+
+    $mail->isHTML(true);                                  // Set email format to HTML
+
+    $mail->Subject = 'A Message From '.$_POST['name'];
+    $mail->Body    = $_POST['message'];
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
+
+
+ ?>
+
+
+
 <!DOCTYPE html>
 <html>
 
